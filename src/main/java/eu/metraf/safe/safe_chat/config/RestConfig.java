029b6b2de86621b68fc5c73d68b58f5ae5952277
artemfirsov <firsov.tyoma@gmail.com>
2018-01-13 01:24:26 +0100

    added rest controller

---
+++ src/main/java/eu/metraf/safe/safe_chat/config/RestConfig.java
@@ -0,0 +1,15 @@
+package eu.metraf.safe.safe_chat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class RestConfig {
+
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+
+}
